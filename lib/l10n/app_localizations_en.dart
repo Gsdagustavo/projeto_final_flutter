a7417de8447f061f7451e8dc8d5484f3dd7b5d2b
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-27 15:14:28 -0300

    feat: create arb files for en, pt and es internationalization and make translation for the enums

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,43 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get test_label => 'Test';
+
+  @override
+  String get transport_type_car => 'Car';
+
+  @override
+  String get transport_type_bike => 'Bike';
+
+  @override
+  String get transport_type_bus => 'Bus';
+
+  @override
+  String get transport_type_plane => 'Plane';
+
+  @override
+  String get transport_type_cruise => 'Cruise';
+
+  @override
+  String get experience_cultural_immersion => 'Cultural Immersion';
+
+  @override
+  String get experience_alternative_cuisines => 'Alternative Cuisines';
+
+  @override
+  String get experience_historical_places => 'Visit Historical Places';
+
+  @override
+  String get experience_visit_local_establishments => 'Visit Local Establishments (Bars, Restaurants, Parks, etc.)';
+
+  @override
+  String get experience_contact_with_nature => 'Contact With Nature';
+}
---
+++ lib/l10n/app_localizations_pt.dart
@@ -0,0 +1,43 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Portuguese (`pt`).
+class AppLocalizationsPt extends AppLocalizations {
+  AppLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get test_label => 'Teste';
+
+  @override
+  String get transport_type_car => 'Carro';
+
+  @override
+  String get transport_type_bike => 'Moto';
+
+  @override
+  String get transport_type_bus => 'Ônibus';
+
+  @override
+  String get transport_type_plane => 'Avião';
+
+  @override
+  String get transport_type_cruise => 'Cruzeiro';
+
+  @override
+  String get experience_cultural_immersion => 'Imersão Cultural';
+
+  @override
+  String get experience_alternative_cuisines => 'Culinárias Alternativas';
+
+  @override
+  String get experience_historical_places => 'Visitar Locais Históricos';
+
+  @override
+  String get experience_visit_local_establishments => 'Visitar Estabelecimentos Locais (Bares, Restaurantes, Parques, etc.)';
+
+  @override
+  String get experience_contact_with_nature => 'Contato com a Natureza';
+}
