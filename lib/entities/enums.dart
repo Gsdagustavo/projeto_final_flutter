e3731700d9e8d036ced1da7a50e897ef6aead29c
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-27 15:12:44 -0300

    feat: create enums for transport types and experiences

---
+++ lib/entities/enums.dart
@@ -0,0 +1,9 @@
+enum TransportTypes { car, bike, bus, plane, cruise }
+
+enum Experiences {
+  cultureImmersion,
+  alternativeCuisines,
+  visitHistoricalPlaces,
+  visitLocalEstablishments,
+  contactWithNature,
+}
