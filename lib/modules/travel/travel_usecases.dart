f3ab74f97448d818df0511a4e2fb6ab123904810
Gsdagustavo <gugadanielalvez@gmail.com>
2025-07-07 21:28:25 -0300

    feat: make a custom exception for travel register and implement it in the usecase:

--- lib/modules/travel/travel_usecases.dart
+++ lib/modules/travel/travel_usecases.dart
@@ -14,29 +14,37 @@ class TravelUseCasesImpl implements TravelUseCases {
   Future<void> registerTravel(Travel travel) async {
     /// No title
     if (travel.travelTitle.trim().isEmpty) {
-      throw Exception('Invalid travel name');
+      throw _TravelRegisterException('Invalid travel name');
     }
 
     /// Invalid start date
     if (travel.startDate == null) {
-      throw Exception('Invalid travel start date');
+      throw _TravelRegisterException('Invalid travel start date');
     }
 
     /// Invalid end date
     if (travel.endDate == null) {
-      throw Exception('Invalid travel end date');
+      throw _TravelRegisterException('Invalid travel end date');
     }
 
     /// No participants
     if (travel.participants.isEmpty) {
-      throw Exception('Travel must contain at least 1 participant');
+      throw _TravelRegisterException(
+        'Travel must contain at least 1 participant',
+      );
     }
 
     /// No stops
     if (travel.stops.isEmpty) {
-      throw Exception('Travel must contain at least 1 stop');
+      throw _TravelRegisterException('Travel must contain at least 1 stop');
     }
 
     await travelRepository.registerTravel(travel: travel);
   }
+}
+
+class _TravelRegisterException implements Exception {
+  final String message;
+
+  _TravelRegisterException(this.message);
 }
