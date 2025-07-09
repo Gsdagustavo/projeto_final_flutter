7effa90b96c51c380e8ab3da0ad5f8932129b17e
Gsdagustavo <gugadanielalvez@gmail.com>
2025-07-09 08:38:27 -0300

    fix: add locale provider in widget tree before using it on main app

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,7 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../entities/enums.dart';
 import '../../l10n/app_localizations.dart';
+import '../../services/locale_service.dart';
+import '../providers/locale_provider.dart';
 import '../util/enums_extensions.dart';
 import 'fab_page.dart';
 
@@ -20,6 +25,23 @@ class HomePage extends StatelessWidget {
         itemCount: items.length,
         itemBuilder: (context, index) {
           return Text(items[index].getIntlTransportType(context));
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final locState = Provider.of<LanguageCodeProvider>(
+            context,
+            listen: false,
+          );
+
+          await locState.changeLanguageCode(
+            languageCode:
+                LocaleService.languageCodes[Random().nextInt(
+                  LocaleService.languageCodes.length,
+                )],
+          );
+          debugPrint(locState.languageCode);
         },
       ),
     );
