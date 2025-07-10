18b443e8f16cd4ae7a407c74defca520346018b0
Gsdagustavo <gugadanielalvez@gmail.com>
2025-07-10 17:24:20 -0300

    feat: make usecase and repository classes to participants

---
+++ lib/modules/participant/participant_repository.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+
+import '../../database/database.dart';
+import '../../database/tables/participants_table.dart';
+import '../../database/tables/travel_participants_table.dart';
+import '../../entities/participant.dart';
+
+abstract class ParticipantRepository {
+  Future<void> registerParticipant(Participant participant, int travelId);
+
+  Future<List<Participant>> getAllParticipants();
+
+  Future<List<Participant>> getParticipantsByTravelId(int travelId);
+}
+
+class ParticipantRepositoryImpl implements ParticipantRepository {
+  late final Future<Database> _db = DBConnection().getDatabase();
+
+  @override
+  Future<void> registerParticipant(
+    Participant participant,
+    int travelId,
+  ) async {
+    final db = await _db;
+
+    await db.insert(ParticipantsTable.tableName, participant.toMap());
+
+    await db.insert(TravelParticipantsTable.tableName, {
+      TravelParticipantsTable.travelId: travelId,
+      TravelParticipantsTable.participantId: participant.id,
+    });
+  }
+
+  @override
+  Future<List<Participant>> getAllParticipants() async {
+    final db = await _db;
+
+    final participants = await db.query(
+      ParticipantsTable.tableName,
+      orderBy: ParticipantsTable.name,
+    );
+
+    return participants.map(Participant.fromMap).toList();
+  }
+
+  @override
+  Future<List<Participant>> getParticipantsByTravelId(int travelId) async {
+    final db = await _db;
+
+    final travelParticipants = await db.rawQuery(
+      'SELECT * FROM ${TravelParticipantsTable.tableName} as TP '
+      'INNER JOIN ${ParticipantsTable.tableName} as P '
+      'ON TP.${TravelParticipantsTable.participantId} = P.${ParticipantsTable.participantId} '
+      'WHERE ${TravelParticipantsTable.travelId} = ?',
+      [travelId],
+    );
+
+    final participantsList = travelParticipants
+        .map(Participant.fromMap)
+        .toList();
+    debugPrint(participantsList.join('\n'));
+
+    return participantsList;
+  }
+}
---
+++ lib/modules/participant/participant_usecases.dart
@@ -0,0 +1,33 @@
+import '../../entities/participant.dart';
+import 'participant_repository.dart';
+
+abstract class ParticipantUseCases {
+  Future<void> registerParticipant(Participant participant, int travelId);
+
+  Future<List<Participant>> getAllParticipants();
+
+  Future<List<Participant>> getParticipantsByTravelId(int travelId);
+}
+
+class ParticipantUseCasesImpl implements ParticipantUseCases {
+  final ParticipantRepositoryImpl _participantRepository;
+
+  ParticipantUseCasesImpl(this._participantRepository);
+
+  @override
+  Future<void> registerParticipant(Participant participant, int travelId) {
+    // TODO: implement registerParticipant
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Participant>> getAllParticipants() async {
+    return await _participantRepository.getAllParticipants();
+  }
+
+  @override
+  Future<List<Participant>> getParticipantsByTravelId(int travelId) async {
+    await _participantRepository.getParticipantsByTravelId(travelId);
+    return [];
+  }
+}
