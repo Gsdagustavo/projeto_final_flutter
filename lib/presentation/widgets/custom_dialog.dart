83126a30139423afcf11e1d8410b64c1a6984eb7
Gsdagustavo <gugadanielalvez@gmail.com>
2025-07-16 11:47:10 -0300

    fix: add CustomDialog widget to vcs

---
+++ lib/presentation/widgets/custom_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  const CustomDialog({
+    super.key,
+    required this.title,
+    this.isError = false,
+    required this.content,
+  });
+
+  final String title;
+  final Widget content;
+  final bool isError;
+
+  @override
+  Widget build(BuildContext context) {
+    final Icon icon;
+
+    if (isError) {
+      icon = Icon(Icons.error, color: Colors.red);
+    } else {
+      icon = Icon(Icons.check, color: Colors.green);
+    }
+
+    return AlertDialog(title: Text(title), icon: icon, content: content);
+  }
+}
