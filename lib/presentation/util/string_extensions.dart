88c0e7b8b9bb4590567b213da51ffc8a6dd4ae31
Gsdagustavo <gugadanielalvez@gmail.com>
2025-07-21 13:41:30 -0300

    feat: make a string extension to get the initial of a string

---
+++ lib/presentation/util/string_extensions.dart
@@ -0,0 +1,3 @@
+extension StringInitialExension on String {
+  String getUppercaseInitial() => this[0].toUpperCase();
+}
--- lib/presentation/util/string_format_utils.dart
+++
@@ -1,7 +0,0 @@
-abstract class StringFormatUtils {
-  static String getStringInitial(String text) {
-    if (text.isEmpty) return '';
-
-    return text[0].toUpperCase();
-  }
-}
