ec6ace870e7640b3208c539b5a8f4e4f3dfab5d1
Gsdagustavo <gugadanielalvez@gmail.com>
2025-07-31 11:39:44 -0300

    feat: add automaticallyImplyLeading argument to my_app_bar widget

--- lib/presentation/widgets/my_app_bar.dart
+++ lib/presentation/widgets/my_app_bar.dart
@@ -5,7 +5,12 @@ import 'theme_toggle_button.dart';
 /// This widget is a custom [AppBar] that is used in [FabPage]
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   /// Constant constructor
-  const MyAppBar({super.key, required this.title, this.actions});
+  const MyAppBar({
+    super.key,
+    required this.title,
+    this.actions,
+    this.automaticallyImplyLeading = false,
+  });
 
   /// The title of the page that will be shown in the
   /// App Bar
@@ -14,10 +19,12 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
   /// A list of widgets of all actions of the App Bar
   final List<Widget>? actions;
 
+  final bool automaticallyImplyLeading;
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      automaticallyImplyLeading: false,
+      automaticallyImplyLeading: automaticallyImplyLeading,
       // leading: null,
       title: Text(
         title,
