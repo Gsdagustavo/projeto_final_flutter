ec91b098a6b06f5f5014d2b231a6243bc22d45ec
Gsdagustavo <gugadanielalvez@gmail.com>
2025-08-02 13:29:48 -0300

    refactor: improve ui in home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import '../../core/extensions/date_extensions.dart';
@@ -81,27 +82,63 @@ class _TravelWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final locale = Localizations.localeOf(context).toString();
+    return Container(
+      padding: const EdgeInsets.all(32.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.green,
+      ),
 
-    return Column(
-      // mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          travel.startDate!.getFormattedDate(locale),
-          style: TextStyle(fontSize: 24),
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(Icons.circle_outlined, color: Colors.blue),
+              Padding(padding: EdgeInsets.all(6)),
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  border: BoxBorder.all(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                height: 50,
+                child: Text(
+                  travel.stops.first.place.display,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
 
-        Padding(padding: EdgeInsets.all(12)),
+          Icon(Icons.route),
 
-        Row(
-          children: [
-            Text('${travel.startDate!.hour}:${travel.startDate!.minute}'),
-          ],
-        ),
+          Row(
+            children: [
+              Icon(Icons.pin_drop, color: Colors.red),
+              Padding(padding: EdgeInsets.all(6)),
 
-        _ParticipantsWidget(participants: travel.participants),
-      ],
+              Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  border: BoxBorder.all(color: Colors.black, width: 1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                height: 50,
+                child: Text(
+                  travel.stops.last.place.display,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+
+          Padding(padding: EdgeInsets.all(12)),
+
+          _ParticipantsWidget(participants: travel.participants),
+        ],
+      ),
     );
   }
 }
@@ -130,30 +167,8 @@ class _ParticipantsWidget extends StatelessWidget {
             ),
           ),
 
-          // Container(
-          //   width: 100,
-          //   height: 100,
-          //   color: Colors.red,
-          // )
-
-          // for (final (idx, participant) in participants.indexed)
-          //   Positioned(
-          //     right: (20 * idx) + 40,
-          //     child: CircleAvatar(
-          //       backgroundImage: FileImage(participants[i].profilePicture),
-          //     ),
-          //   ),
-
-          // for (int i = 0; i < participants.length && i < 3; i++)
-          //   Positioned(
-          //     right: (20 * i) + 40,
-          //     child: CircleAvatar(
-          //       backgroundImage: FileImage(participants[i].profilePicture),
-          //     ),
-          //   ),
-          //
-          // if (participants.length > 3)
-          //   CircleAvatar(radius: 20, child: Text('...')),
+          if (participants.length > 2)
+            CircleAvatar(radius: 20, child: Text('...')),
         ],
       ),
     );
