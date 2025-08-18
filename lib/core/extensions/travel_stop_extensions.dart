d81bb6ddfc67f5b8655850b6e65546269f4d84fd
Gsdagustavo <gugadanielalvez@gmail.com>
2025-08-18 14:59:00 -0300

    feat: implement an extension to get a MarkerId from a TravelStop

---
+++ lib/core/extensions/travel_stop_extensions.dart
@@ -0,0 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../domain/entities/travel_stop.dart';
+
+extension TravelStopExtensions on TravelStop {
+  MarkerId toMarkerId() {
+    return MarkerId('${place.latitude},${place.longitude}');
+  }
+}
