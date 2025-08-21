f195bf73cb1a0074f07ba568ae196cdb5b597cd4
Gsdagustavo <gugadanielalvez@gmail.com>
2025-08-21 16:35:34 -0300

    fix: update build.gradle.kts to avoid build errors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 }
 
 android {
+
     namespace = "com.example.projeto_final_flutter"
     compileSdk = flutter.compileSdkVersion
 
@@ -19,11 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.projeto_final_flutter"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = 33
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -31,12 +29,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
+
 
 flutter {
     source = "../.."
