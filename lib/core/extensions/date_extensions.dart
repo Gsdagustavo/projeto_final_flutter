31e629ae3dc8c32195ab7b9a68b7628c9de276be
Gsdagustavo <gugadanielalvez@gmail.com>
2025-08-24 10:56:52 -0300

    feat: add an extension method to get a formatted date

--- lib/core/extensions/date_extensions.dart
+++ lib/core/extensions/date_extensions.dart
@@ -1,17 +1,21 @@
 import 'package:intl/intl.dart';
 
 extension DateExtensions on DateTime {
+  String getMonthDay(String locale) {
+    final formatter = DateFormat('MMM d', locale);
+    final formatted = formatter.format(this);
+    return formatted[0].toUpperCase() + formatted.substring(1);
+  }
+
   String getFormattedDate(String locale) {
     final formatter = DateFormat('EE, dd MMMM', locale);
     final formatted = formatter.format(this);
-
     return formatted[0].toUpperCase() + formatted.substring(1);
   }
 
   String getFormattedDateWithYear(String locale) {
     final formatter = DateFormat('EE, dd MMMM yyyy', locale);
     final formatted = formatter.format(this);
-
     return formatted[0].toUpperCase() + formatted.substring(1);
   }
 }
